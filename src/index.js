01658ed18a4b1294fcd188edd6634b2411871911
yamin029 <55071387+yamin029@users.noreply.github.com>
2021-04-15 13:13:16 +0600

    complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
 //Create a React app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const currentHour = new Date().getHours();
+let text = "";
+//console.log(currentHour);
+
+const color = {
+  color: ""
+};
+
+if (currentHour < 12) {
+  console.log(currentHour + "->1 to 12");
+  text = "Morning";
+  color.color = "red";
+} else if (currentHour < 18) {
+  console.log(currentHour + "->13 to 18");
+  text = "Afternoon";
+  color.color = "green";
+} else {
+  console.log(currentHour + "->19 to 24");
+  text = "Evening";
+  color.color = "blue";
+}
+console.log(color);
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" style={color}>
+      Good {text}
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
 //Show a single h1 that says "Good morning" if between midnight and 12PM.
 //or "Good Afternoon" if between 12PM and 6PM.
 //or "Good evening" if between 6PM and midnight.
